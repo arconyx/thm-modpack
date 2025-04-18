4910bbfadcaab435045a96f5f886860d394f428b
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-04-18 15:06:10 +1200

    add packwiz as nix dev shell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Packwiz environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ packwiz ];
+        };
+      }
+    );
+}
